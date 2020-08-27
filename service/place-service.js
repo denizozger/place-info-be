d29533e8695583ca1d928fd38ed703600849f3d8
Deniz Ozger <denizozger@gmail.com>
2020-08-27 21:22:56 +0200

    remove redundant getThirdPartyApiUrlForPlaceId function

--- service/place-service.js
+++ service/place-service.js
@@ -3,12 +3,8 @@ const fetch = require("node-fetch");
 const CODING_SESSION_API_ENDPOINT =
   "https://storage.googleapis.com/coding-session-rest-api";
 
-function getThirdPartyApiUrlForPlaceId(placeId) {
-  return `${CODING_SESSION_API_ENDPOINT}/${placeId}`;
-}
-
 async function getPlace(placeId) {
-  return (await fetch(getThirdPartyApiUrlForPlaceId(placeId))).json();
+  return (await fetch(`${CODING_SESSION_API_ENDPOINT}/${placeId}`)).json();
 }
 
 module.exports = {
