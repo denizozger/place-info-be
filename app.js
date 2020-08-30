2ebe8e2be54e3b34b89b7000738666197dd72523
Deniz Ozger <denizozger@gmail.com>
2020-08-30 16:16:36 +0200

    move transformPlace() to place-service.js

--- app.js
+++ app.js
@@ -2,25 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const placeService = require("./service/place-service");
-const { populateMissingDays } = require("./helpers/time-utils");
 
 const app = express();
 app.use(cors());
-
-function transformPlace(placeId, place) {
-  return {
-    id: placeId,
-    name: place.displayed_what,
-    address: place.displayed_where,
-    openingHours: populateMissingDays(place.opening_hours.days),
-  };
-}
 
 app.get("/places/:placeId", async (req, res) => {
   const placeId = req.params.placeId;
 
   const place = await placeService.getPlace(placeId);
-  const transformedPlace = transformPlace(placeId, place);
+  const transformedPlace = placeService.transformPlace(placeId, place);
 
   return res.json(transformedPlace);
 });
--- service/place-service.js
+++ service/place-service.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const { populateMissingDays } = require("../helpers/datetime-utils");
 
 const CODING_SESSION_API_ENDPOINT =
   "https://storage.googleapis.com/coding-session-rest-api";
@@ -7,6 +8,16 @@ async function getPlace(placeId) {
   return (await fetch(`${CODING_SESSION_API_ENDPOINT}/${placeId}`)).json();
 }
 
+function transformPlace(placeId, place) {
+  return {
+    id: placeId,
+    name: place.displayed_what,
+    address: place.displayed_where,
+    openingHours: populateMissingDays(place.opening_hours.days),
+  };
+}
+
 module.exports = {
   getPlace,
+  transformPlace,
 };
